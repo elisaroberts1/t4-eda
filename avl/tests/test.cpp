71aad8cb3a1e55bcb7e7f5dc22f7271f3e658ca6
Elisa Roberts N <elisarobertsn@iMac-de-Elisa.local>
2022-11-19 22:38:43 -0300

    min_heap

--- avl/tests/test.cpp
+++ avl/tests/test.cpp
@@ -14,39 +14,111 @@ using namespace eda;
 using namespace trees;
 using namespace std;
 
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+class node {       // The class
+  private:
+    string nodeid;
+    int val;  
+    class node * ptr = nullptr;  
+  public:
+    node(int v, string y) { 
+        val = v;
+        nodeid = y;
+        }
+    int getVal(){
+        return val;
+    }
+    string getNodeid(){
+        return nodeid;
+    }
+    void set_nextNode(node *nodeptr){
+        ptr = nodeptr;
+    }
+    node* get_Nodeptr(){
+        return ptr;
+    }
+
+};
 
 
-void dijkstra(LinkedList a, LinkedList d, int v_final, int v_inicial, int cant){
+class min_Heap {      
+    private:
+        class node * primerNodo = nullptr;
+    public:             
+        void insert(node *nodo){
+            bool listo = false;
+            node* tmp = primerNodo;
+            node * tmp_dsp = nullptr;
+
+            if (tmp == nullptr){ // caso en que no haya nada guardado en heap
+                listo = true;
+                primerNodo = nodo;
+                
+            }
+            else if( nodo->getVal()<= tmp->getVal()){
+                nodo->set_nextNode(tmp);
+                primerNodo = nodo;
+                listo = true;
+            }
+            else{
+                node* tmp_dps = tmp->get_Nodeptr();
+            }
+            while (!listo) {
+                if (nodo->getVal() <= tmp_dsp->getVal()){
+                    tmp->set_nextNode(nodo);
+                    nodo->set_nextNode(tmp_dsp);
+                    listo = true;
+                }   
+                else {
+                    if ( tmp_dsp == nullptr){
+                        tmp->set_nextNode(nodo);
+                        listo = true;
+                    }
+                    else{
+                        tmp = tmp_dsp;
+                        tmp_dsp = tmp_dsp->get_Nodeptr();
+                    }
+                }
+            }
+        }
+        node* top(){
+            return primerNodo;
+        }
+};
+
+
+/*void dijkstra(LinkedList a, LinkedList d, int v_final, int v_inicial, int cant){
 	int A[cant];
 	int D[cant];
-	//MIN_HEAP
+	Stack H;
 	for(int i=0; i < cant; i++){
 		D[i] = 10000;
 		A[i] = v_inicial;
 	}
 	D[v_inicial] = 0;
-	//H.push();
+	H.push(v_inicial);
 	int j = 1;
-	/*while(!H.empty()){
-		int q = H.top();
+	while(!H.isEmpty()){
+		int q = H.top()->getData();
 		H.pop();
-		found = False;
-		bool a[v_inicial].find(v_final){
-			found = True;
+		bool a[v_inicial]->find(v_final){
+			return found = true;
 		}
-		if(found == True){
-			int p = a[v_inicial].pos(v_final);
-			Node* f = D[v_inicial].find_val_pos(p);
+		int u[a[v_inicial].size()];
+		int o[d[v_inicial].size()];
+		for(int  i=0; i < a[v_inicial].size(); i++){
+			u[i] = a[v_inicial].find_val_pos(i);
 		}
-		else{
-			A[j] = 
+		int y=0;
+		while(y<a[v_inicial].size()){
+			cout << u[y] << endl;
+			y++;
 		}
-		for(int i=0; i <a[q].size() ; i++){
-			int distancia=;
-		}
+		
 	}
-	*/
-}
+}*/
 
 
 int main(int nargas, char** vargs){
@@ -105,17 +177,35 @@ int main(int nargas, char** vargs){
 		A[v_ini].insertFirst(v_fi);
 		D[v_ini].insertFirst(di);
 	}
+
+	for(int i=0; i < cant_v; i++){
+		cout << "La lista enlazada de vertices adyacentes al vertice " << i << " corresponde a: ";
+		A[i].print();
+		cout << endl << "Las distancias correspondientes a cada una de esas aristas son: ";
+		D[i].print();
+		cout << endl << endl;
+	}
+
 	int p = A[3].pos(1);
 	Node* f = D[3].find_val_pos(p);
-	Stack stack;
-	stack.push(1);
-	stack.push(10);
-	stack.push(5);
-	cout << "hola"<<endl;
-	stack.min_heap();
-	while(!stack.isEmpty()){
-		cout << stack.top()->getData() << endl;
-		stack.pop();
-	}
+
+    node ejemplo(3,"1");
+    node*ptrej;
+    node ejemplo2(4,"3");
+    node ejemplo3(1,"4");
+    ptrej = &ejemplo2;
+
+    cout<< "El nodoid es: " << ejemplo.getNodeid()<< endl;
+    ejemplo.set_nextNode(ptrej);
+    cout << "Ejemplo 1 apunta al nodo: " << ejemplo.get_Nodeptr()->getNodeid() << endl;
+    
+    min_Heap heap;
+    heap.insert(ptrej);
+    node * ptr2 = &ejemplo;
+	heap.insert(ptr2);
+    node * ptr3 = &ejemplo3;
+    heap.insert(ptr3);
+    cout<< "El top es: "<< heap.top()->getVal() << endl;
+
 	return 0;
 }
